87135e1ab36998846b3cf697c0d11445f7b8b1a3
Vicky Daiya <37933641+vickydaiya@users.noreply.github.com>
2022-04-20 19:33:34 -0400

    Final

---
+++ analysis.R
@@ -0,0 +1,32 @@
+#install.packages("openxlsx")
+
+library(data.table)
+library(openxlsx)
+
+# tsv_dir <- paste0(getwd(),"/Azimuth TSVs/")
+# tsv_files <- list.files(tsv_dir)
+# 
+# data_list <- list()
+# for (tsv_file in tsv_files)
+# {
+#   data = read.delim(paste0(tsv_dir,tsv_file), row.names = 1)
+#   data <- data.table(data)
+#   data_summary <- data[,list(count = .N, percentage = (.N/nrow(data))*100),by='predicted.cluster']
+#   data_list <- append(data_list,list(data_summary))
+# }
+# names(data_list) = tsv_files
+# write.xlsx(data_list, file = "d",headerStyle = createStyle(textDecoration = "Bold"), colWidths = "auto")
+
+tsv_dir <- paste0(getwd(),"/Azimuth local TSVs/")
+tsv_files <- list.files(tsv_dir)
+
+data_list <- list()
+for (tsv_file in tsv_files)
+{
+  data = read.delim(paste0(tsv_dir,tsv_file), row.names = 1)
+  data <- data.table(data)
+  data_summary <- data[,list(count = .N, percentage = (.N/nrow(data))*100),by='predicted.cluster']
+  data_list <- append(data_list,list(data_summary))
+}
+names(data_list) = tsv_files
+write.xlsx(data_list, file = "Azimuth_local_analysis.xlsx",headerStyle = createStyle(textDecoration = "Bold"), colWidths = "auto")
